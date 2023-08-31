aa6e64ebb3b24e691b4c81f0894a09120043c3a2
Jener Sigua <jener.sigua31@gmail.com>
2023-09-01 01:59:38 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+})
+
+module.exports = withPWA({
+})
