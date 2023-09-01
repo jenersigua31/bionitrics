14b1d7c4a04ba94e9f1e1d554191a2317cd8e4bf
Jener Sigua <jener.sigua31@gmail.com>
2023-09-01 13:04:28 +0800

    3

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
- 
-import withPWA from 'next-pwa';
-import runtimeCaching from 'next-pwa/cache.js';
-const isProduction = process.env.NODE_ENV === 'production';
- 
-const config = {
-  // here goes your Next.js configuration
-};
- 
-const nextConfig = withPWA({
-  dest: 'public',
-  disable: !isProduction,
-  runtimeCaching
-})(
-  config
-);
- 
-export default nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
+  // next.js config
+})
